8b7ec5d629ca58bf342eef0bef73c50c3e6fa15a
WadeCappa <wade.cappa@msn.com>
2024-01-23 21:40:23 -0500

    mpi orchestrator

---
+++ src/mpi_find_approximation_set.cpp
@@ -0,0 +1,18 @@
+#include "data_tools/normalizer.h"
+#include "data_tools/matrix_builder.h"
+#include "representative_subset_calculator/naive_representative_subset_calculator.h"
+#include "representative_subset_calculator/lazy_representative_subset_calculator.h"
+#include "representative_subset_calculator/fast_representative_subset_calculator.h"
+#include "representative_subset_calculator/orchestrator/single_machine_orchestrator.h"
+
+#include <CLI/CLI.hpp>
+#include "nlohmann/json.hpp"
+
+
+int main(int argc, char** argv) {
+    CLI::App app{"Approximates the best possible approximation set for the input dataset."};
+    Orchestrator* orchestrator = SingleMachineOrchestrator::build(argc, argv, app);
+    CLI11_PARSE(app, argc, argv);
+
+    return 0;
+}
