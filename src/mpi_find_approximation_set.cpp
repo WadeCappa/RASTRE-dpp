356d79279864cce2789d6229081f8acc17b7c15c
Wade Cappa <wade.cappa@msn.com>
2024-01-29 17:46:32 -0800

    finished raw implementation

--- src/mpi_find_approximation_set.cpp
+++ src/mpi_find_approximation_set.cpp
@@ -1,6 +1,5 @@
 #include "data_tools/normalizer.h"
 #include "data_tools/matrix_builder.h"
-#include "logger/logger.h"
 #include "representative_subset_calculator/naive_representative_subset_calculator.h"
 #include "representative_subset_calculator/lazy_representative_subset_calculator.h"
 #include "representative_subset_calculator/fast_representative_subset_calculator.h"
@@ -22,8 +21,6 @@ int main(int argc, char** argv) {
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &appData.worldRank);
     MPI_Comm_size(MPI_COMM_WORLD, &appData.worldSize);
-
-    MpiLogger logger(appData.worldRank);
 
     unsigned int seed = (unsigned int)time(0);
     MPI_Bcast(&seed, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
