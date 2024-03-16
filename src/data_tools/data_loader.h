007c23bd734ba99948f76c2ce77c1f55e97aa31d
Wade Cappa <wade.cappa@msn.com>
2024-03-16 12:03:24 -0700

    finished adjacency list loader

--- src/data_tools/data_loader.h
+++ src/data_tools/data_loader.h
@@ -17,6 +17,89 @@ class DataLoader {
     virtual bool getNext(std::vector<double> &result) = 0;
 };
 
+class AsciiAdjacencyListDataLoader : public DataLoader {
+    private:
+    const static size_t EXPECTED_ELEMENTS_PER_LINE = 3;
+
+    const size_t totalColumns;
+
+    long expectedRow;
+    long currentRow;
+    double value = 1.0; 
+    long to;
+    bool hasData;
+    std::istream &source;
+
+    public:
+    AsciiAdjacencyListDataLoader(
+        std::istream &input, 
+        const size_t totalColumns
+    ) : 
+        source(input), 
+        expectedRow(0), 
+        hasData(false),
+        totalColumns(totalColumns) 
+    {}
+
+    bool getNext(std::vector<double> &result) {
+        std::string data;
+        result.clear();
+        result.resize(this->totalColumns, 0);
+
+        while (true) {
+            if (this->hasData) {
+                if (this->currentRow == this->expectedRow) {
+                    result[to] = value;
+                } else {
+                    this->expectedRow++;
+                    return true;
+                }
+            }
+
+            if (!std::getline(this->source, data)) {
+                if (this->hasData) {
+                    this->hasData = false;
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            this->hasData = false;
+
+            std::istringstream stream(data);
+            long number;
+            size_t totalSeen = 0;
+
+            // The expected format is a two ints a line, and 
+            //  maybe a third reprsenting value.
+            while(stream >> number && totalSeen < EXPECTED_ELEMENTS_PER_LINE) {
+                switch (totalSeen) {
+                    case 0:
+                        currentRow = number;
+                    case 1:
+                        to = number;
+                    case 2:
+                        value = number;
+                }
+
+                if (stream.peek() == ',')
+                    stream.ignore();
+
+                totalSeen++;
+            }
+            
+            if (currentRow == this->expectedRow) {
+                result[to] = value;
+            } else {
+                this->expectedRow++;
+                this->hasData = true;
+                return true;
+            }
+        } 
+    }
+};
+
 class AsciiDataLoader : public DataLoader {
     private:
     std::istream &source;
