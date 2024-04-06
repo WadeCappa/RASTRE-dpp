8d5f36a9fbfecaafbe071056ec4068f8da2b278e
Wade Cappa <wade.cappa@msn.com>
2024-04-06 15:32:03 -0700

    now using visitors

--- src/data_tools/data_row.h
+++ src/data_tools/data_row.h
@@ -2,11 +2,82 @@
 #include <vector>
 
 static const std::string DELIMETER = ",";
+
+class DataRowVisitor {
+    public:
+    virtual void visitDenseDataRow(const std::vector<double>& data) = 0;
+    virtual void visitSparseDataRow(const std::map<size_t, double>& data) = 0;
+};
+
+class DenseDotProductDataRowVisitor : public DataRowVisitor {
+    private:
+    double res;
+    const std::vector<double>& base;
+
+    public:
+    DenseDotProductDataRowVisitor(const std::vector<double>& base) : base(base), res(0) {}
+
+    void visitDenseDataRow(const std::vector<double>& data) {
+        double result = 0;
+        for (size_t i = 0; i < this->base.size() && i < data.size(); i++) {
+            res += this->base[i] * data[i];
+        }
+
+        this->res = result;
+    }
+
+    void visitSparseDataRow(const std::map<size_t, double>& data) {
+        double result = 0;
+        for (const auto & p : data) {
+            result += this->base[p.first] * p.second;
+        }
+
+        this->res = result;
+    }
+
+    double get() const {
+        return this->res;
+    }
+};
+
+class SparseDotProductDataRowVisitor : public DataRowVisitor {
+    private:
+    double res;
+    const std::map<size_t, double>& base;
+
+    public:
+    SparseDotProductDataRowVisitor(const std::map<size_t, double>& base) : base(base), res(0) {}
+
+    void visitDenseDataRow(const std::vector<double>& data) {
+        double result = 0;
+        for (const auto & p : this->base) {
+            result += data[p.first] * p.second;
+        }
+
+        this->res = result;
+    }
+
+    void visitSparseDataRow(const std::map<size_t, double>& data) {
+        double result = 0;
+        for (const auto & p : this->base) {
+            if (data.find(p.first) != data.end()) {
+                result += data.at(p.first) * p.second;
+            }
+        }
+
+        this->res = result;
+    }
+
+    double get() const {
+        return this->res;
+    }
+};
+
 class DataRow {
     public:
-    virtual const std::vector<double> getRow() const = 0;
-    virtual double dotProduct(const DataRow& dataRow) const = 0;
     virtual size_t size() const = 0;
+    virtual void visit(DataRowVisitor &visitor) const = 0;
+    virtual double dotProduct(const DataRow& dataRow) const = 0;
 }; 
 
 class DenseDataRow : public DataRow {
@@ -18,12 +89,18 @@ class DenseDataRow : public DataRow {
     
     DenseDataRow(std::vector<double> data) : data(move(data)) {}
 
-    const std::vector<double> getRow() const {
-        return data;
-    }
-
     size_t size() const {
         return this->data.size();
+    }
+
+    double dotProduct(const DataRow& dataRow) const {
+        DenseDotProductDataRowVisitor visitor(this->data);
+        dataRow.visit(visitor);
+        return visitor.get();
+    }
+
+    void visit(DataRowVisitor &visitor) const {
+        visitor.visitDenseDataRow(this->data);
     }
 };
 
@@ -34,17 +111,17 @@ class SparseDataRow : public DataRow {
 
     SparseDataRow(std::map<size_t, double> map, size_t totalCoumns) : rowToValue(move(map)), totalColumns(totalColumns) {}
 
-    const std::vector<double> getRow() const {
-        std::vector<double> res(this->totalColumns, 0);
-        for (auto it = this->rowToValue.begin(); it != this->rowToValue.end(); it++)
-        {
-            res[it->first] = it->second;
-        } 
-
-        return move(res);
-    }
-
     size_t size() const {
         return this->totalColumns;
+    }
+
+    double dotProduct(const DataRow& dataRow) const {
+        SparseDotProductDataRowVisitor visitor(this->rowToValue);
+        dataRow.visit(visitor);
+        return visitor.get();
+    }
+
+    void visit(DataRowVisitor &visitor) const {
+        visitor.visitSparseDataRow(this->rowToValue);
     }
 };
