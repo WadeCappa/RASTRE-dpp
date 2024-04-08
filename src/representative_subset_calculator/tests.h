11a6a590f7b1f9908bce18b6ee2f224af55a8a98
Wade Cappa <wade.cappa@msn.com>
2024-04-07 17:47:17 -0700

    finished calculator tets

--- src/representative_subset_calculator/tests.h
+++ src/representative_subset_calculator/tests.h
@@ -14,54 +14,55 @@
 static const size_t k = DENSE_DATA.size();
 static const double epsilon = 0.01;
 
-// static std::unique_ptr<Subset> testCalculator(SubsetCalculator *calculator) {
-//     std::unique_ptr<Subset> res = calculator->getApproximationSet(data, k);
+static std::unique_ptr<Subset> testCalculator(SubsetCalculator *calculator) {
+    std::unique_ptr<FullyLoadedData> data(FullyLoadedData::load(DENSE_DATA));
+    std::unique_ptr<Subset> res = calculator->getApproximationSet(*data, k);
 
-//     CHECK(res->size() == k);
-//     std::set<size_t> seen;
-//     for (const auto & seed : *res.get()) {
-//         const auto & row = seed;
-//         CHECK(seen.find(row) == seen.end());
-//         seen.insert(row);
-//     }
+    CHECK(res->size() == k);
+    std::set<size_t> seen;
+    for (const auto & seed : *res.get()) {
+        const auto & row = seed;
+        CHECK(seen.find(row) == seen.end());
+        seen.insert(row);
+    }
 
-//     delete calculator;
-//     return move(res);
-// }
+    delete calculator;
+    return move(res);
+}
 
-// TEST_CASE("Testing Naive representative set finder") {
-//     testCalculator(new NaiveSubsetCalculator());
-// }
+TEST_CASE("Testing Naive representative set finder") {
+    testCalculator(new NaiveSubsetCalculator());
+}
 
-// TEST_CASE("Testing lazy-naive set finder") {
-//     testCalculator(new LazySubsetCalculator());
-// }
+TEST_CASE("Testing lazy-naive set finder") {
+    testCalculator(new LazySubsetCalculator());
+}
 
-// TEST_CASE("Testing FAST set finder") {
-//     testCalculator(new FastSubsetCalculator(epsilon));
-// }
+TEST_CASE("Testing FAST set finder") {
+    testCalculator(new FastSubsetCalculator(epsilon));
+}
 
-// TEST_CASE("Testing LAZYFAST set finder") {
-//     testCalculator(new LazyFastSubsetCalculator(epsilon));
-// }
+TEST_CASE("Testing LAZYFAST set finder") {
+    testCalculator(new LazyFastSubsetCalculator(epsilon));
+}
 
-// void checkSolutionsAreEquivalent(const Subset &a, const Subset &b) {
-//     CHECK(a.size() == b.size());
-//     for (size_t i = 0; i < a.size() && i < b.size(); i++) {
-//         CHECK(a.getRow(i) == b.getRow(i));
-//     }
+void checkSolutionsAreEquivalent(const Subset &a, const Subset &b) {
+    CHECK(a.size() == b.size());
+    for (size_t i = 0; i < a.size() && i < b.size(); i++) {
+        CHECK(a.getRow(i) == b.getRow(i));
+    }
 
-//     CHECK(a.getScore() > b.getScore() - LARGEST_ACCEPTABLE_ERROR);
-//     CHECK(a.getScore() < b.getScore() + LARGEST_ACCEPTABLE_ERROR);
-// }
+    CHECK(a.getScore() > b.getScore() - LARGEST_ACCEPTABLE_ERROR);
+    CHECK(a.getScore() < b.getScore() + LARGEST_ACCEPTABLE_ERROR);
+}
 
-// TEST_CASE("All calculators have the same result") {
-//     auto naiveRes = testCalculator(new NaiveSubsetCalculator());
-//     auto lazyRes = testCalculator(new LazySubsetCalculator());
-//     auto fastRes = testCalculator(new FastSubsetCalculator(epsilon));
-//     auto lazyFastRes = testCalculator(new LazyFastSubsetCalculator(epsilon));
+TEST_CASE("All calculators have the same result") {
+    auto naiveRes = testCalculator(new NaiveSubsetCalculator());
+    auto lazyRes = testCalculator(new LazySubsetCalculator());
+    auto fastRes = testCalculator(new FastSubsetCalculator(epsilon));
+    auto lazyFastRes = testCalculator(new LazyFastSubsetCalculator(epsilon));
 
-//     checkSolutionsAreEquivalent(*naiveRes.get(), *lazyRes.get());
-//     checkSolutionsAreEquivalent(*lazyRes.get(), *fastRes.get());
-//     checkSolutionsAreEquivalent(*fastRes.get(), *lazyFastRes.get());
-// }
+    checkSolutionsAreEquivalent(*naiveRes.get(), *lazyRes.get());
+    checkSolutionsAreEquivalent(*lazyRes.get(), *fastRes.get());
+    checkSolutionsAreEquivalent(*fastRes.get(), *lazyFastRes.get());
+}
