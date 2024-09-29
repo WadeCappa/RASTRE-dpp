5cdc0d309c265789cfd51db5cc6db77b24599064
Reet Barik <reetbarik94@gmail.com>
2024-09-29 15:25:30 -0700

    Writing PU and CU data to a file that can be used as input to UserMode

--- scripts/UserMode/ConstructPuAndCu.py
+++ scripts/UserMode/ConstructPuAndCu.py
@@ -3,17 +3,24 @@ import numpy as np
 from scipy.sparse import coo_matrix
 from sklearn.metrics.pairwise import cosine_similarity
 import multiprocessing as mp
+import sys
 
-topN = 10
-topU = 5
+topN = int(sys.argv[1])
+topU = int(sys.argv[2])
 # Read CSV file into a pandas DataFrame
-dataDir = '/global/cfs/cdirs/m1641/DPP_MAP/'
-df = pd.read_csv(dataDir + 'MovieLens/MovieLens.txt', header=None, delim_whitespace=True)  # Assuming no header in the CSV
-df.columns =['Items', 'Users'] #ease of use
+filepath_input = sys.argv[3]
+filepath_output = sys.argv[4]
+df = pd.read_csv(filepath_input, header=None, delim_whitespace=True)  # Assuming no header in the CSV
 
 print("Dataset read")
 
-data = [1] * len(df)  # 1 for each non-zero entry
+if (df.shape[1] == 2):
+    df.columns =['Items', 'Users'] #ease of use
+    data = [1] * len(df)  # 1 for each non-zero entry
+else:
+    df.columns =['Items', 'Users', 'data'] #ease of use
+    data = df['data']
+
 rows = df['Items']
 cols = df['Users']
 
@@ -26,6 +33,11 @@ print("Similarity matrix constructed")
 
 # Dict of items rated by every user
 PU_allUsers = df.groupby('Users')['Items'].apply(set).to_dict()
+
+PU_test = {}
+for user,items in PU_allUsers.items():
+    PU_test[user] = items.pop()
+
 # Retain only topU active users
 PU = dict(sorted(PU_allUsers.items(), key=lambda x: len(x[1]), reverse=True)[:topU])
 
@@ -36,6 +48,8 @@ CU = {}
 RU = {}
 
 for user,items in PU.items(): 
+    if (PU_test[user] in items):
+        print("This shouldn't be happening")
     C_user = set()
     for item in PU[user]: 
         similaritiesPerElement = similarities[item].toarray().flatten()  # Convert sparse row to dense array
@@ -49,42 +63,28 @@ for user,items in PU.items():
         C_user.update(top_n_indices)
     
     CU[user] = list(C_user)
+    print("PU size:")
+    print(len(PU[user]))
+    print("CU size:")
+    print(len(CU[user]))
     
 print("CU constructed")
 
-# for user in PU.keys():
-#     P_items = list(PU[user])
-#     C_items = list(CU[user])
-    
-#     R_user = [0] * len(C_items)
-#     for i, item in enumerate(C_items):
-#     # Compute the sum of similarities between elements of P_items and C_items
-#         R_user[i] = sum(similarities[item, y] for y in P_items)
-    
-#     RU[user] = R_user
+LU = {}
 
-# Worker function to compute similarities for a single user
-# def process_user(user_data):
-#     user, P_items, C_items = user_data
-#     R_user = []
-#     for item in C_items:
-#         total_similarity = sum(similarities[item, y] for y in P_items)
-#         R_user.append(total_similarity)
-#     return user, R_user
+with open(filepath_output, "ab") as f:
+    for user,items in PU.items():
+        l = []
+        l.append(user)
+        l.append(PU_test[user])
+        l.append(len(PU[user]))
+        l.extend(list(PU[user]))
+        l.append(len(CU[user]))
+        l.extend(CU[user])
 
-# # Parallel computation using multiprocessing Pool
+        LU[user] = l
+        print("LU size:")
+        print(len(LU[user]))
+        np.savetxt(f, [np.array(l)], fmt='%d', delimiter=' ')
 
-# # Prepare the data for each user to pass to the worker function
-# user_data = [(user, list(PU[user]), list(CU[user])) for user in PU.keys()]
-
-# # Create a pool of workers
-# with mp.Pool(32) as pool:
-#     # Map the worker function to the data
-#     results = pool.map(process_user, user_data)
-
-# # Collect the results in the final dictionary
-# RU = {user: R_user for user, R_user in results}
-    
-
-# print("RU constructed")
 
