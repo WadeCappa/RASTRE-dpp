85d6c42631313fbf444188672a76d581eb49edc2
Wade Cappa <wade.cappa@msn.com>
2024-12-01 13:57:32 -0800

    added usermode cmd

--- src/representative_subset_calculator/orchestrator/orchestrator.h
+++ src/representative_subset_calculator/orchestrator/orchestrator.h
@@ -36,6 +36,7 @@ struct appData{
     unsigned int threeSieveT;
     float alpha = 1;
     bool stopEarly = false;
+    std::string userModeFile;
 
     int worldSize = 1;
     int worldRank = 0;
@@ -110,6 +111,7 @@ class Orchestrator {
         app.add_flag("--loadBinary", appData.binaryInput, "Use this flag if you want to load a binary input file.");
         app.add_flag("--normalizeInput", appData.normalizeInput, "Use this flag to normalize each input vector.");
         app.add_flag("--stopEarly", appData.stopEarly, "Used excusevly during streaming to stop the execution of the program early. If you use this in conjuntion with randgreedi, you will lose your approximation guarantee");
+        app.add_option("-u,--userModeFile", appData.userModeFile, "Path to user mode data. Only set this if you are processing a dataset for a set of users.");
     
         CLI::App *loadInput = app.add_subcommand("loadInput", "loads the requested input from the provided path");
         CLI::App *genInput = app.add_subcommand("generateInput", "generates synthetic data");
