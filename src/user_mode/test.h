f959c69b794852c589b9d326c9a5b6a22699171d
Wade Cappa <wade.cappa@msn.com>
2025-01-20 00:49:31 -0500

    added user mode load test

--- src/user_mode/test.h
+++ src/user_mode/test.h
@@ -1,6 +1,30 @@
 
-
+#include <string>  
+#include <iostream> 
+#include <sstream>  
 
 TEST_CASE("Testing LAZYFAST set finder") {
-    std::cout << "hello world" << std::endl;
+    const double u_id = 11;
+    const double t_id = 54;
+    const double l_cu = 3;
+    const std::vector<double> cu({43.000, 99.000, 4532.0000});
+    const std::vector<double> ru({4.124, 16.6213, 9.2313});
+    std::stringstream data;
+    data << u_id << " " << t_id << " " << l_cu << " ";
+    for (const auto cu_i : cu) {
+        data << cu_i << " ";
+    }
+    for (const auto ru_i : ru) {
+        data << ru_i << " ";
+    }
+
+    std::vector<std::unique_ptr<UserData>> users (UserDataImplementation::load(data));
+    CHECK(users.size() == 1);
+    std::unique_ptr<UserData> user(move(users[0]));
+    CHECK(user->getUserId() == u_id);
+    CHECK(user->getTestId() == t_id);
+    for (size_t i = 0; i < l_cu; i++) {
+        CHECK(user->getCu()[i] == cu[i]);
+        CHECK(user->getRu()[i] == ru[i]);
+    }
 }
