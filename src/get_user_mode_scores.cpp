23c8364b838b66c0874e1a26b31c2c349fd132c1
Wade Cappa <wade.cappa@msn.com>
2025-03-23 17:34:15 -0400

    improved scoring

--- src/get_user_mode_scores.cpp
+++ src/get_user_mode_scores.cpp
@@ -54,6 +54,7 @@ int main(int argc, char** argv) {
     if (appData.loadInput.inputFile != EMPTY_STRING) {
         inputFile.close();
     } 
+    data->print_DEBUG();
 
     spdlog::info("Finished loading dataset of size {0:d} ...", data->totalRows());
 
@@ -91,7 +92,9 @@ int main(int argc, char** argv) {
                     continue;
                 }
 
-                scores[j * ru.size() + i] = 1.0 - calc->get(i, j);
+                const float v = 1.0 - calc->get(i, j);
+                SPDLOG_TRACE("i of {0:d} and j of {1:d} got score of {2:f}", i, j, v);
+                scores[j * ru.size() + i] = v;
                 if (scores[j * ru.size() + i] > 1.0) {
                     spdlog::error("score of {0:f} > 1 for j {1:d} and i {2:d}", scores[j * ru.size() + i], j, i);
                 }
@@ -105,7 +108,9 @@ int main(int argc, char** argv) {
             ilmd = std::min(ilmd, s);
         }
         // we subtract the diagonals since we don't include them in our score
-        double ilad = aggregate_scores / (std::pow(ru.size(), 2) - (double)ru.size());
+        const double total_number_of_pairs = (std::pow(ru.size(), 2) - (double)ru.size());
+        SPDLOG_DEBUG("Going to divide {0:f} by the total number of pairs of {1:f}", aggregate_scores, total_number_of_pairs);
+        double ilad = aggregate_scores / total_number_of_pairs;
         spdlog::info("User {0:d}: MRR = {1:f}, ILAD = {2:f}, ILMD = {3:f}", user_id, mrr, ilad, ilmd);
         total_mrr += mrr;
         total_ilad += ilad; 
