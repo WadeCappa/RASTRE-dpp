b50684d5a035ce4f59845f2e77cb39fb9ce5082a
Wade Cappa <wade.cappa@msn.com>
2025-03-30 15:34:45 -0400

    compile in parallel

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ LOG_LEVEL="${1:-$Info}"
 module load mpi/openmpi-x86_64
 echo "using log level ${LOG_LEVEL}"
 cmake . -DCMAKE_TOOLCHAIN_FILE=build/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DLOG_LEVEL=${LOG_LEVEL}
-cmake --build .
+cmake --build . -j
 ./tools/run_tests
